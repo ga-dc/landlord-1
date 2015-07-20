900f4d16d29e950044770043350dc5aa6b3ec6ab
Nicholas Olds <nolds9@yahoo.com>
2015-07-20 18:02:52 -0400

    Adding completed hw

--- models/tenant.rb
+++ models/tenant.rb
@@ -1,5 +1,14 @@
-class Tenenant
+# require 'pry'
+
+class Tenant
   attr_accessor :name, :age, :gender
 
-
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
 end
+
+
+# puts "exercise complete" # fixes a bug in pry
