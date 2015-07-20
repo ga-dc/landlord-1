6cac60c4a9d443e1682549623529cdff3cf09921
Alex Bannon <alexbannon@gmail.com>
2015-07-20 18:11:57 -0400

    working apartment tenant classes with rspec tests

--- models/Tenant.rb
+++ models/Tenant.rb
@@ -1,9 +1,8 @@
 class Tenant
-  attr_reader :name
-  attr_reader :age
-  attr_reader :gender
+  attr_reader :name, :age, :gender
   def initialize(name, age, gender)
     @name = name.capitalize
     @age = age
     @gender = gender
   end
+end
