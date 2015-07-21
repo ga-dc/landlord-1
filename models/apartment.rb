92652a80b22caba7c08c7d6812158d30f8f5eab6
Rosel Padilla <roselpadilla.mail@gmail.com>
2015-07-20 22:50:01 -0400

    Completed Hw

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,14 +1,22 @@
 Class Apartment
 
-  def initialize (adress, monthly_rent, sqft, num_beds, num_baths)
+  def initialize (adress, monthly_rent, sqft, num_beds, num_baths, renters)
     @adress = adress
     @monthly_rent = monthly_rent
     @sqft = sqft
     @num_beds = num_beds
     @num_baths = num_baths
+    @renters = renters
   end
 
-  def add_tenant
+  def add_tenant (someone)
+
+    if @renters.length < @num_beds
+      @renters.push(someone)
+    else
+      puts "Too many people, not enough beds fam."
+    end
+
   end
 
 end
