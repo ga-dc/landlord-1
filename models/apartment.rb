d4631af2ea890f44b425d84dbdffa655ef490338
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-07-21 08:52:14 -0400

    all committed

---
+++ models/apartment.rb
@@ -0,0 +1,18 @@
+class Apartment
+
+  attr_accessor :address, :monthly_rent, :sqft, :num_beds, :num_baths, :renters
+
+  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
+    @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @renters = renters
+  end
+
+  def add_tenant(new_tenant)
+      @renters << new_tenant if @renters.length < @num_beds
+  end
+
+end
