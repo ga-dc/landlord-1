7ab55f7ac8e33daa72c7ea4e70445e4e5a041e32
MikeDulik <mikedulik@gmail.com>
2015-07-21 08:29:31 -0400

    functional, passes tests

---
+++ models/Tenant.rb
@@ -0,0 +1,10 @@
+
+class Tenant
+  attr_accessor :name, :age, :gender
+
+  def initialize(initial_name, initial_age, initial_gender)
+    @name = initial_name
+    @age = initial_age
+    @gender = initial_gender
+  end
+end
