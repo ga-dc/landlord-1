a77028f1154c8e8f5fee0f652d30b6e3dceaf0df
Kevin Cole Jr <KevinColeLr@gmail.com>
2015-07-21 08:44:58 -0400

    inital commit

---
+++ models/apartment.rb
@@ -0,0 +1,9 @@
+class Apartment
+  attr_accessor :address, :monthly_rent, :sqft, :num_beds, :num_baths, :renters
+  def @add_tenant
+      if :renters < :num_beds
+      return :renters = :renters + 1
+      else
+        return
+      end
+  end
