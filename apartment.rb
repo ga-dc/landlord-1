45c1536974bac155c710c1f21086588b4de1b853
Lisa <lscottcook@hotmail.com>
2015-07-21 08:54:33 -0400

    update

--- apartment.rb
+++ apartment.rb
@@ -1,15 +1,47 @@
 class Apartment
 def initialize(address)
-   @name= address
-   @gladiators= []
+   @address= []
+   @monthly_rent= []
+   sqft= []
+   num_beds= []
+   num_baths= []
+   renters = []
+
   end
 
-address (a single string including floor and unit numbers if applicable)
-monthy_rent
-sqft
-num_beds
-num_baths
-renters (the tenant or tenants living in the apartment)
+  def address
+  return @address.to_s
+  end
 
+  def monthly_rent
+  return @monthly_rent.to_i
+  end
+
+
+  def sqft
+  return @sqft.to_i
+  end
+
+
+  def num_beds
+  return @num_beds.to_i
+  end
+
+  def num_baths
+  return @num_baths.to_i
+  end
+
+
+  def renters
+  return @renters.to_i
+  end
+
+
+def add_tentant
+  tentant= 0
+  until (tentant < num_beds) do
+    tentant ++
+
+end
 
 end # end class
