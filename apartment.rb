19fa461cb94e75c8e5ad889fd54f57af7b702abb
Lisa <lscottcook@hotmail.com>
2015-07-21 07:11:01 -0400

    classes added

---
+++ apartment.rb
@@ -0,0 +1,15 @@
+class Apartment
+def initialize(address)
+   @name= address
+   @gladiators= []
+  end
+
+address (a single string including floor and unit numbers if applicable)
+monthy_rent
+sqft
+num_beds
+num_baths
+renters (the tenant or tenants living in the apartment)
+
+
+end # end class
---
+++ tenant.rb
@@ -0,0 +1,20 @@
+class tentant.rb
+def initialize(name)
+   @name= name.capitalize
+   @age= []
+   @gender=[]
+  end
+
+  def name
+  return @name
+  end
+
+  def age
+  return @age
+  end
+
+  def gender
+  return @gender
+  end
+
+end #end class
