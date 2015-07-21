92a5b8061fb55600559eb8504204712c0d3eea65
Justin Price <Justin@Justins-MacBook-Pro.local>
2015-07-21 08:56:24 -0400

    added methods which include all variables required in apartment.rb

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,11 +1,15 @@
 class Apartment
   attr_reader :renters, :monthly_rent, :sqft, :num_beds, :num_baths
+    def initalize(renters, monthly_rent)
     @renters = []
     @monthly_rent = monthly_rent
+  end
+    def initlialize(sqft)
     @sqft = sqft
+  end
+    def initialize(num_beds, num_baths)
     @num_beds = num_beds
     num_baths = num_baths
-
   end
 
   def add_tenant(renter)
@@ -13,6 +17,4 @@ class Apartment
     if renters.length > num_beds.length
       renters.pop
     end
-
-
-end
+  end
