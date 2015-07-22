320e714406d18243a926488489a4f6e8d9161fbc
Silvia Brown <siltastic@gmail.com>
2015-07-21 23:56:00 -0400

    Solved all failures.

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,12 +1,13 @@
 class Apartment
   attr_accessor :address, :monthly_rent, :sqft, :num_beds, :num_baths, :renters
-  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
-    @address = address
-    @monthly_rent = rent
-    @sqft = sqft
-    @num_beds = num_beds
-    @num_baths = num_baths
-    @renters = renters
+  
+  def initialize (address, monthly_rent, sqft, num_beds, num_baths, renters)
+   @address = address
+   @monthly_rent = monthly_rent
+   @sqft = sqft
+   @num_beds = num_beds
+   @num_baths = num_baths
+   @renters = renters
   end
 
   def add_tenant(tenant)
